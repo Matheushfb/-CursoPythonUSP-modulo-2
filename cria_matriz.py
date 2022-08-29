deae02bf05ec91c26eadd65f1e4ac152a707ee30
matheus <matheushfbrandao@gmail.com>
2022-08-29 09:20:35 -0300

    exemplos de codigo

---
+++ cria_matriz.py
@@ -0,0 +1,11 @@
+def cria_matriz(tot_lin, tot_col, valor):
+    matriz = []  #lista vazia
+    for i in range(tot_lin):
+        linha = []
+        for j in range(tot_col):
+            linha.append(valor)
+        matriz.append(linha)
+    return matriz
+
+
+cria_matriz(3,3,0)
