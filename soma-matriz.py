6d2fbda238d7544808ef14f322d16bcbe8b72a00
matheus <matheushfbrandao@gmail.com>
2022-09-14 23:54:47 -0300

    soma-matriz

--- soma-matriz.py
+++ soma-matriz.py
@@ -1,13 +1,21 @@
-def soma_matriz(m1, m2):
+def soma_matrizes(m1, m2):
     m3 = []
-    for i in range(len(m1)):
-        s1 = m1[i]
-        s2 = m2[i]
-        for j in range(len(s1)):
-            m3.append(s1[j]+s2[j])
-    return print(m3)
+    linha = []
+    if len(m1) != len(m2):
+        return False
+    else:
+        for i in range(len(m2)):
+                s = m1[i]
+                s2 = m2[i]
+                for j in range(len(s)):
+                    linha.append(s[j] + s2[j])
+                m3.append(linha)
+                linha = []
+    return m3
 
 
-#m1 = [[1,2,3],[3,2,3]]
-#m2 = [[1,1,1],[1,1,1]]
-#print(soma_matriz(m1,m2))
+m1 = []
+m2 = []
+#m1 = [[2],[1]]
+#m2 = [[5],[1]]
+soma_matrizes(m1,m2)
