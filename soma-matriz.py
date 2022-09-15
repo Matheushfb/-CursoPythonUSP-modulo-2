bb263e75422ac4db841bfeec72830c0cabfb0618
matheus <matheushfbrandao@gmail.com>
2022-09-15 14:41:07 -0300

    adição de testes unitarios

--- soma-matriz.py
+++ soma-matriz.py
@@ -2,7 +2,7 @@ def soma_matrizes(m1, m2):
     m3 = []
     linha = []
     if len(m1) != len(m2):
-        return False
+        return 0
     else:
         for i in range(len(m2)):
                 s = m1[i]
@@ -13,9 +13,16 @@ def soma_matrizes(m1, m2):
                 linha = []
     return m3
 
+def test_soma():
+    assert soma_matrizes([[1,2,3],[1,2,3]],[[3,2,1],[3,2,1]]) == [[4,4,4],[4,4,4]]
+def test_matrizes_dif():
+    assert soma_matrizes([[1,2,3],[1,2,3]],[[3,2,1]]) == 0
+def teste_matriz_unidimensional():
+    assert soma_matrizes([[1]], [[3]]) == [[4]]
 
-m1 = []
-m2 = []
+
+m1 = [[1,2,3],[1,2,3]]
+m2 = [[3,2,1],[3,2,1]]
 #m1 = [[2],[1]]
 #m2 = [[5],[1]]
-soma_matrizes(m1,m2)
+print(soma_matrizes(m1,m2))
