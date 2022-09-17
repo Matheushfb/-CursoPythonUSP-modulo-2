18195c8601091c5ca01ad0e576f27ef241561bed
matheus <matheushfbrandao@gmail.com>
2022-09-16 23:58:31 -0300

    devolve menor nome captalized

---
+++ menor_nome.py
@@ -0,0 +1,20 @@
+def menor_nome(lista_de_nomes):
+    menor = 99
+    menor_temp = ""
+    menor_nome = ""
+    for i in lista_de_nomes:
+        menor_temp = i.strip()
+        if len(menor_temp) < menor:
+            menor = len(menor_temp)
+            menor_nome = menor_temp
+        else:
+            menor_temp = menor_temp
+    return menor_nome.capitalize()
+
+def test_nome():
+    assert menor_nome(['marta','tania','     ana',]) == 'Ana'
+
+
+lista_de_nomes = ['marta','tania','     ana']
+print(menor_nome(lista_de_nomes))
+
