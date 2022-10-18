ce1b3a2804329207ac8f285e32f9617ec886b7a5
matheus <matheushfbrandao@gmail.com>
2022-10-18 20:15:34 -0300

    codigo que informa se uma lista está ordenada ou não

---
+++ Ordenado.py
@@ -0,0 +1,16 @@
+def ordenada(lista):
+    count = 0
+    for i in range(len(lista)):
+#        print(i)
+#        print(lista[i+1])
+        if len(lista) <= 1:
+            return True
+        if lista[i] < lista[i+1]:
+            count = count + 1
+            if count == len(lista) - 1:
+                return True
+        else:
+            return False
+
+#lista = [1]
+#print(ordenada(lista))
