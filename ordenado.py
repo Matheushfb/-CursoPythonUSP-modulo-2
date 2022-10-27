b27297ed83024d92909e88529072632c493c2966
matheus <matheushfbrandao@gmail.com>
2022-10-27 09:13:11 -0300

    melhoria na logica do algoritmo

--- ordenado.py
+++ ordenado.py
@@ -1,14 +1,10 @@
 def ordenada(lista):
-    count = 0
-    for i in range(len(lista)):
+    for i in range(len(lista)-1):
         if len(lista) <= 1:
             return True
-        if lista[i] < lista[i+1]:
-            count = count + 1
-            if count == len(lista) - 1:
-                return True
-        else:
+        if lista[i] > lista[i+1]:
             return False
+    return True
 
-#lista = [1]
-#print(ordenada(lista))
+lista = [1,2,3,4]
+print(ordenada(lista))
